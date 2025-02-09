525dcc073a61c14975e1de7925ba6f390ccfd3ea
Hafizhun Soekiswo <hsoekiswo@gmail.com>
2025-02-09 15:37:14 +0800

    fix: back nav function

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,4 +1,4 @@
-import { NavLink } from "react-router";
+import { NavLink, useNavigate } from "react-router";
 
 export function Nav() {
     return (
@@ -62,11 +62,15 @@ export function HomeNav() {
 }
 
 export function BackNav() {
+  const navigate = useNavigate();
+
   return (
     <nav className="flex flex-row items-center justify-start border pl-2 py-3 fixed top-0 left-0 w-full bg-white opacity-90">
-      <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="currentColor" className="bi bi-arrow-left" viewBox="0 0 16 16">
-        <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8"/>
-      </svg>
+      <button onClick={() => {navigate(-1);}}>
+        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" className="bi bi-arrow-left" viewBox="0 0 16 16">
+          <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8"/>
+        </svg>
+      </button>
     </nav>
   )
 }
